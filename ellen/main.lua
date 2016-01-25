f7ad42768bbd1d6771af72d691df58d40a628b63
Andy Gayton <andy@thecablelounge.com>
2016-01-24 19:56:49 -0800

    add jump to beginning of line and end of line. on escape, move cursor back one

--- ellen/main.lua
+++ ellen/main.lua
@@ -20,7 +20,13 @@ local line = ellen.spans()
 
 local modes = {
 	function(ch)
-		if ch == ESC then return 2 end
+		if ch == ESC then
+			if x > 1 then
+				x = x - 1
+				term:move_left()
+			end
+			return 2
+		end
 		line:put(ch)
 		io.write(ch)
 		x = x + 1
@@ -28,6 +34,18 @@ local modes = {
 	end,
 
 	function(ch)
+		if ch == "0" then
+			if x > 1 then
+				term:move_left(x-1)
+				x = 1
+			end
+		end
+		if ch == "$" then
+			if x < #line then
+				term:move_right(#line - x)
+				x = #line
+			end
+		end
 		if ch == "h" then
 			if x > 1 then
 				x = x - 1
