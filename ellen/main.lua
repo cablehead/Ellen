f9fe04de1d80459ca5dab2fa41b10cab8c22d03f
Andy Gayton <andy@thecablelounge.com>
2016-01-25 20:54:24 -0800

    work towards multi-line support; move status bar to bottom line of window

--- ellen/main.lua
+++ ellen/main.lua
@@ -1,25 +1,26 @@
 local levee = require("levee")
 local _ = require("levee")._
 
+io.stdout:setvbuf('no')  -- unbuffer stdout
 
 local ellen = require("ellen")
 
-
-io.stdout:setvbuf('no')  -- unbuffer stdout
-
-
 local ESC = "\27"
-
 
 local term = ellen.term()
 
-
 local x = 0
-local line = ellen.line()
+local y = 1
+
+
+local lines = {
+	ellen.line(), }
 
 
 local modes = {
 	function(ch)
+		local line = lines[y]
+
 		if ch == ESC then
 			return 2
 		end
@@ -33,18 +34,14 @@ local modes = {
 	end,
 
 	function(ch)
-		if ch == "0" then
-			x = 1
-		end
-		if ch == "$" then
-			x = #line
-		end
-		if ch == "h" then
-			x = x - 1
-		end
-		if ch == "l" then
-			x = x + 1
-		end
+		local line = lines[y]
+
+		if ch == "0" then x = 1 end
+		if ch == "$" then x = #line end
+		if ch == "j" then y = y + 1 end
+		if ch == "k" then y = y - 1 end
+		if ch == "h" then x = x - 1 end
+		if ch == "l" then x = x + 1 end
 		if ch == "i" then x = x - 1 return 1 end
 		if ch == "q" then return 0 end
 		return 2
@@ -52,35 +49,48 @@ local modes = {
 }
 
 
-local function main()
-	local err, size = _.term.winsize(1)
-	print(_.repr(size))
-
-	local h = levee.Hub()
-
-	local err, reset, stream = ellen.raw(h)
-
-	term:clear()
-
-	local mode = 1
-
-	while true do
-		term:move(1, 2)
-		term:EL()
-		io.write(("mode: %d x: %d"):format(mode, x))
-		term:move(mode == 1 and x + 1 or x, 1)
-		local err, ch = stream:recv()
-		mode = modes[mode](ch)
-		if mode == 0 then break end
-		if x < 0 then x = 0 end
-		if x > #line then x = #line end
-	end
-
-	reset()
-	term:move(1, 2)
+local function status(row, s, ...)
+	term:move(1, row)
 	term:EL()
-	print(line:tail(0))
+	io.write(s:format(...))
 end
 
 
-main()
+local function main(h, ws, stream)
+	local mode = 1
+	while true do
+		status(ws.row, "mode: %s x: %s y:%s", mode, x, y)
+		term:move(mode == 1 and x + 1 or x, y)
+		local err, ch = stream:recv()
+		mode = modes[mode](ch)
+		if mode == 0 then break end
+
+		if y < 1 then y = 1 end
+		if y > #lines then y = #lines end
+		local line = lines[y]
+		if x < 0 then x = 0 end
+		if x > #line then x = #line end
+	end
+end
+
+
+local h = levee.Hub()
+
+local err, reset, stream = ellen.raw(h)
+assert(not err)
+
+local err, ws = _.term.winsize(1)
+term:clear()
+
+local status, rc = xpcall(
+	function() return main(h, ws, stream) end,
+	function(err) return debug.traceback() .. "\n\n" .. err end)
+
+reset()
+term:move(1, ws.row)
+io.write("\n")
+
+if status then os.exit(rc) end
+
+print(rc)
+os.exit(1)
