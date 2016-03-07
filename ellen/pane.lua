244da36ad0b9b13734d17d044528a0345a7aa8d1
Andy Gayton <andy@thecablelounge.com>
2016-03-06 16:05:40 -0800

    commit pane

---
+++ ellen/pane.lua
@@ -0,0 +1,32 @@
+local Pane_mt = {}
+Pane_mt.__index = Pane_mt
+
+
+function Pane_mt:render(term, lines)
+	local h = 0
+	while h < self.h do
+		if h < #lines then
+			local line = lines[h + 1] or ""
+			local compare = self.lines[h + 1] or ""
+			if line.peek then line = line:peek(self.w) end
+			if line ~= compare then
+				term:move(self.x, self.y + h)
+				term:EL(2)
+				io.write(line)
+			end
+			self.lines[h + 1] = line
+		end
+		h = h + 1
+	end
+end
+
+
+return function(x, y, w, h)
+	local self = setmetatable({}, Pane_mt)
+	self.x = x
+	self.y = y
+	self.w = w
+	self.h = h
+	self.lines = {}
+	return self
+end
