1851d2990b53d0edd03b1d6b9ef13d6d82a80124
Andy Gayton <andy@thecablelounge.com>
2016-03-12 23:21:14 -0800

    sketching

--- ellen/layout.lua
+++ ellen/layout.lua
@@ -1,6 +1,18 @@
+[[
+
+                                       │
+                                       │
+                                       │
+                                       │
+                                       │
+                                       │
+                                       │
+status bar: status ────────────────────┴─────────────────────────
+]]
 local hr = "─"
 local vr = "│"
 local aw = "┼"
 local sd = "┬"
+local su = "┴"
 local sr = "├"
 local sl = "┤"
